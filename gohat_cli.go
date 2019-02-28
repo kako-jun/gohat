63a2494b0dc6df74c56515b33eadc9f802945967
kako-jun <3541096+kako-jun@users.noreply.github.com>
2019-02-28 23:42:44 +0900

    update.

--- gohat_cli.go
+++ gohat_cli.go
@@ -8,7 +8,7 @@ import (
 	"./gohat"
 )
 
-func parseArgs() ( command string, commandArgs []string, err error) {
+func parseArgs() (shPath string, commandArgs []string, err error) {
 	flag.Parse()
 	if flag.NArg() < 1 {
 		err = errors.New("invalid argument")
@@ -16,21 +16,20 @@ func parseArgs() ( command string, commandArgs []string, err error) {
 	}
 
 	args := flag.Args()
-	command = args[0]
+	shPath = args[0]
 	commandArgs = args[1:]
 	return
 }
 
 // entry point
 func main() {
-	 command, args, err := parseArgs()
+	shPath, args, err := parseArgs()
 	if err != nil {
 		fmt.Println("error:", err)
 		fmt.Println("usage:")
-		fmt.Println("  gohat [command]")
-		fmt.Println("  gohat [command] [command options]")
+		fmt.Println("  gohat [.sh path]")
 		return
 	}
 
-	gohat.Exec(command, args)
+	gohat.Exec(shPath, args)
 }
